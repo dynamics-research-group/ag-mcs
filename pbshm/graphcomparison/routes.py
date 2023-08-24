15c79ffac6c3ce5ac33a528ed527c6120ce140e3
Dan Brennan <d.s.brennan@sheffield.ac.uk>
2023-08-24 14:40:25 +0100

    Ordering of structures within the heatmap
    Honour the new ordering passed from the templates for the order in
    which the strutures should be compared and thusly be presented within
    the displayed heatmap. Provide additional diagnostic information to the
    templates being rendered.

--- pbshm/graphcomparison/routes.py
+++ pbshm/graphcomparison/routes.py
@@ -1,10 +1,9 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request
 from pbshm.authentication.authentication import authenticate_request
 from pbshm.pathfinder.pathfinder import population_list
 from pbshm.db import structure_collection
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import pbshm.graphcomparison.filehandling as fh
-import pandas as pd
+from pbshm.pathfinder.pathfinder import nanoseconds_since_epoch_to_datetime
+from pbshm.graphcomparison.matrix import create_similarity_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 import io
@@ -20,64 +19,70 @@ def list():
 	return population_list("graphcomparison.generate")
 
 @bp.route("/generate/<population>")
-@authenticate_request("graphcomparison-load-the-page")
+@authenticate_request("graphcomparison-list-structures")
 def generate(population):
 	documents = []
 	for document in structure_collection().aggregate([
 		{"$match": {
 			"population": population, 
-			"models":{"$exists":True}
+			"models": {"$exists": True}
 		}},
 		{"$project": {
 			"_id": 0,
-			"timestamp": 0,
-			"population": 0,
-			"models": 0
+			"name": 1,
+			"population": 1, 
+			"timestamp": 1,
+			"elements": {"$size": "$models.irreducibleElement.elements"},
+			"relationships": {"$size": "$models.irreducibleElement.relationships"}
 		}}
 	]):
+		document["date"] = nanoseconds_since_epoch_to_datetime(document["timestamp"]).strftime("%d/%m/%Y %H:%M:%S")
 		documents.append(document)
+		print(document)
 	return render_template("compare.html", structures=documents)
 
 @bp.route("/compare", methods=["POST"])
 def compare():
+	#Acquire Inputs
 	form = request.form
 	name_list = form.getlist("structure_selection")
+	name_order = [f"{form.get(f'structure_order_{name}')}:{name}" for name in name_list]
+	sorted_name_list = [name.split(':', 1)[-1] for name in sorted(name_order)]
+	#Load Models
 	structure_list = []
-	for name in name_list:
+	for name in sorted_name_list:
 		for document in structure_collection().aggregate([
 			{"$match": {
 				"name": name
 			}},
-			{"$limit": 1},
 			{"$project": {
 				"_id": 0,
-				"timestamp": 0,
-				"population": 0
-			}}
+				"name": 1,
+				"models": 1
+			}},
+			{"$limit": 1}
 		]):
 			structure_list.append(document)
-	similarity_matrix, nodes_in_mcs = fh.create_distance_matrix(structure_list)
-
-	jaccard_index_dataframe = pd.DataFrame(data=similarity_matrix, index=name_list, columns=name_list).round(2)
-	nodes_in_mcs_dataframe = pd.DataFrame(data=nodes_in_mcs, index=name_list, columns=name_list).astype(int)
-
-	#plt.pyplot.switch_backend('Agg')
-	size = math.floor(len(name_list) * 1.25) if len(name_list) > 4 else 7
-	fig, ax = plt.subplots(figsize=(size, size))
-	sns_plot = sns.heatmap(jaccard_index_dataframe, annot=True, cmap="summer_r", ax=ax)
-	sns_plot.figure.tight_layout()
-
-	FigureCanvas(fig)
+	#Generate Similarity Matrix
+	similarity_matrix, nodes_in_mcs, results_list = create_similarity_matrix(structure_list, structure_list)
+	#Prepare Heatmap
+	size = math.floor(len(name_list) * 1.15) if len(name_list) > 4 else 7
+	fig, ax = plt.subplots(figsize=(size, size), dpi=300)
+	sns_ax = sns.heatmap(
+		similarity_matrix, annot=True, fmt="0.3f",
+		xticklabels=sorted_name_list, yticklabels=sorted_name_list,
+		cbar=True, cmap="viridis", ax=ax
+	)
+	sns_ax.figure.tight_layout()
+	#Save to IO stream
 	img = io.BytesIO()
-	fig.savefig(img)
+	fig.savefig(img, format="png")
 	img.seek(0)
-
-	img_byte = img.getvalue()
-
+	#Render Template
 	return render_template("results.html", 
-		name_list=name_list, 
-		img_result=base64.b64encode(img_byte).decode(),
-		nodes_in_mcs_table=[nodes_in_mcs_dataframe.to_html(classes=["data", "table", "table-sm", "table-bordered"])],
-		jaccard_index_table=[jaccard_index_dataframe.to_html(classes=["data", "table", "table-sm", "table-bordered"])],
-		titles=jaccard_index_dataframe.columns.values
+		original_structure_list=sorted_name_list,
+		comparison_structure_list=sorted_name_list,
+		img_result=base64.b64encode(img.read()).decode(),
+		jaccard_index_matrix=similarity_matrix,
+		nodes_in_mcs_matrix=nodes_in_mcs
 	)
