8b915dd465b34562969e7d5454c3368692ec8860
Dan Brennan <d.s.brennan@sheffield.ac.uk>
2023-08-29 16:01:39 +0100

    Include the comparison type within the UI

--- pbshm/graphcomparison/routes.py
+++ pbshm/graphcomparison/routes.py
@@ -3,7 +3,7 @@ from pbshm.authentication.authentication import authenticate_request
 from pbshm.pathfinder.pathfinder import population_list
 from pbshm.db import structure_collection
 from pbshm.pathfinder.pathfinder import nanoseconds_since_epoch_to_datetime
-from pbshm.graphcomparison.matrix import create_similarity_matrix
+from pbshm.graphcomparison.matrix import ComparisonType, create_similarity_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 import io
@@ -38,13 +38,13 @@ def generate(population):
 	]):
 		document["date"] = nanoseconds_since_epoch_to_datetime(document["timestamp"]).strftime("%d/%m/%Y %H:%M:%S")
 		documents.append(document)
-		print(document)
-	return render_template("compare.html", structures=documents)
+	return render_template("compare.html", structures=documents, comparison_types=ComparisonType)
 
 @bp.route("/compare", methods=["POST"])
 def compare():
 	#Acquire Inputs
 	form = request.form
+	comparison_type_value = form.get("comparison_type")
 	name_list = form.getlist("structure_selection")
 	name_order = [f"{form.get(f'structure_order_{name}')}:{name}" for name in name_list]
 	sorted_name_list = [name.split(':', 1)[-1] for name in sorted(name_order)]
@@ -64,7 +64,8 @@ def compare():
 		]):
 			structure_list.append(document)
 	#Generate Similarity Matrix
-	similarity_matrix, nodes_in_mcs, results_list = create_similarity_matrix(structure_list, structure_list)
+	comparison_type = ComparisonType(int(comparison_type_value))
+	similarity_matrix, nodes_in_mcs, results_list = create_similarity_matrix(structure_list, structure_list, comparison_type)
 	#Prepare Heatmap
 	size = math.floor(len(name_list) * 1.15) if len(name_list) > 4 else 7
 	fig, ax = plt.subplots(figsize=(size, size), dpi=300)
@@ -82,6 +83,7 @@ def compare():
 	return render_template("results.html", 
 		original_structure_list=sorted_name_list,
 		comparison_structure_list=sorted_name_list,
+		comparison_type=comparison_type,
 		img_result=base64.b64encode(img.read()).decode(),
 		jaccard_index_matrix=similarity_matrix,
 		nodes_in_mcs_matrix=nodes_in_mcs
